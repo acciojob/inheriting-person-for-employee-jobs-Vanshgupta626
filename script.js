1c61e401978ea852c694761820585a6e9b66e1e0
Vansh gupta <vanshg6532@gmail.com>
2026-04-03 19:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-// complete this js code
 function Person(name, age) {
     this.name = name;
     this.age = age;
 }
 
-// method using prototype
 Person.prototype.greet = function () {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
-    Person.call(this, name, age); // inherit properties
+    Person.call(this, name, age);
     this.jobTitle = jobTitle;
 }
 
-// inherit prototype
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// method for Employee
 Employee.prototype.jobGreet = function () {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
-
 
 // Do not change code below this line
 window.Person = Person;
