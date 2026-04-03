8f73c673a09f9d307b660b3fdff09b90a448399b
Vansh gupta <vanshg6532@gmail.com>
2026-04-03 19:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old.");
 };
 
 function Employee(name, age, jobTitle) {
@@ -16,13 +16,8 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function () {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
 };
-const p = new Person("Alice", 25);
-p.greet();
-
-const e = new Employee("Bob", 30, "Manager");
-e.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
