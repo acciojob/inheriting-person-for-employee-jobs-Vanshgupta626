ec39e61811901268b2d6b1394905ae8e9b08c66e
Vansh gupta <vanshg6532@gmail.com>
2026-04-09 08:18:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-function Person(name, age) {
+class Person {
+  constructor(name, age) {
     this.name = name;
     this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
 
-Person.prototype.greet = function () {
-    console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old.");
-};
-
-function Employee(name, age, jobTitle) {
-    Person.call(this, name, age);
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);
     this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
 }
-
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-// 👇 IMPORTANT: override greet
-Employee.prototype.greet = function () {
-    console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
-};
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
